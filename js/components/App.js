6ed03730c354306494b195ff9b347a1839820899
Andrew Kwon <askwon@ucdavis.edu>
2016-11-13 14:01:36 -0800

    Remove all and start from scratch ¯\_(ツ)_/¯

---
+++ js/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+class Lune extends React.Component {
+  render () {
+    return (
+      <View>
+        <Text>Wrow.jpg</Text>
+      </View>
+    )
+  }
+}
+
+export default Lune
